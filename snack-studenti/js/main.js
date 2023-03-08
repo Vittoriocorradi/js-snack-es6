5c30538a117b8c55af5ab374efd40887f0221ef0
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-08 15:44:16 +0100

    Snack 2 Lista studenti (id, nome, somma)

--- snack-studenti/js/main.js
+++ snack-studenti/js/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const students = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        gradesSum: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        gradesSum: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        gradesSum: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        gradesSum: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        gradesSum: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        gradesSum: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        gradesSum: 84
+    },
+]
+
+console.log(students);
