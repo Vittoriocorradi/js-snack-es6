939c4bfb0b9c64e67e50f7489b12a5d7ba787b7a
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-08 15:07:53 +0100

    Snack 1 creata lista oggetti

--- snack-grande-gatsby/js/main.js
+++ snack-grande-gatsby/js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+const invitati = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+]
+
+const finalList = invitati.map((element) => {
+    element = ({name: element});
+    return element;
+});
+
+console.log(invitati);
+console.log(finalList);
+
