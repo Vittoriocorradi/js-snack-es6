bc921de8fd192be6bf6799845e6e6a60421c6583
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-09 13:56:15 +0100

    Lista biciclette

--- snack-biciclette/js/main.js
+++ snack-biciclette/js/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const bikeList = [
+    {
+        nome: 'Elops',
+        peso: 10.4
+    },
+    {
+        nome: 'Triban',
+        peso: 9.2
+    },
+    {
+        nome: 'Alpina',
+        peso: 11.3
+    },
+    {
+        nome: 'Riverside',
+        peso: 10.2
+    },
+    {
+        nome: 'Rockrider',
+        peso: 8.9
+    },
+    {
+        nome: 'Holanda',
+        peso: 9.7
+    }
+]
