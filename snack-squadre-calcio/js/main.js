16aff0f6b584f50a19def2afb8e8965a5006ee02
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-09 16:02:26 +0100

    Snack 4 lista squadre

--- snack-squadre-calcio/js/main.js
+++ snack-squadre-calcio/js/main.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const teamsList = [
+    {
+        nome: 'KS Rakow Czestochowa',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Lech Poznan',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Legia Warszawa',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Pogon Szczecin',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Piast Gliwice',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Lechia Gdansk',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Gornik Zabrze',
+        punti: 0,
+        'falli-subiti': 0 
+    },
+    {
+        nome: 'Wisla Plock',
+        punti: 0,
+        'falli-subiti': 0 
+    }
+]
